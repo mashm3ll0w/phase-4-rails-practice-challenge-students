9f1a7801a332e464c726dd69650822468ae0654f
Charles Swaleh <charlswall@live.com>
2023-05-01 18:12:10 +0300

    Completed creating the routes for the Instructor model

--- app/controllers/instructors_controller.rb
+++ app/controllers/instructors_controller.rb
@@ -1,2 +1,29 @@
 class InstructorsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :instructor_not_found
+  def index
+    render json: Instructor.all, status: :ok
+  end
+
+  def show
+    instructor = find_instructor
+    render json: instructor,
+           serializer: InstructorWithStudentsSerializer,
+           status: :ok
+  end
+
+  def destroy
+    instructor = find_instructor
+    instructor.destroy
+    head :no_content
+  end
+
+  private
+
+  def find_instructor
+    Instructor.find(params[:id])
+  end
+
+  def instructor_not_found
+    render json: { error: "Instructor not found" }, status: :not_found
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :students
-  resources :instructors
+  resources :instructors, only: %i[index show destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
